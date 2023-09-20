e616caec8607ae50765663afbac267a481b0bc6e
MahmoudSoliman1998 <mahmoud.soliman121998@gamil.com>
2023-09-20 18:35:34 +0300

    used JSON file to get date

--- src/main/java/gui/nopCommerceDemoStore/pages/LoginPage.java
+++ src/main/java/gui/nopCommerceDemoStore/pages/LoginPage.java
@@ -1,4 +1,29 @@
 package gui.nopCommerceDemoStore.pages;
 
+import com.shaft.driver.SHAFT;
+import org.openqa.selenium.By;
+
 public class LoginPage {
+    // Define driver
+    private SHAFT.GUI.WebDriver driver;
+
+    // Define Locators
+    private static final By EMAIL = By.id("Email");
+    private static final By PASSWORD = By.id("Password");
+    private static final By REMEMBER_ME = By.id("RememberMe");
+    private static final By SUBMIT_LOGIN = By.xpath("//*[@class=\"customer-blocks\"]/div[2]/form/div[3]/button");
+
+    // Constructor
+    public LoginPage(SHAFT.GUI.WebDriver driver)
+    {
+        this.driver = driver;
+    }
+    public void Login(String email, String password)
+    {
+        driver.element().type(EMAIL,email);
+        driver.element().type(PASSWORD,password);
+        driver.element().click(REMEMBER_ME);
+        driver.element().click(SUBMIT_LOGIN);
+    }
+
 }
--- src/test/java/gui/nopCommerceDemoStore/tests/SignUp_POM.java
+++ src/test/java/gui/nopCommerceDemoStore/tests/SignUp_POM.java
@@ -3,6 +3,7 @@ package gui.nopCommerceDemoStore.tests;
 import com.shaft.driver.DriverFactory;
 import com.shaft.driver.SHAFT;
 import gui.nopCommerceDemoStore.pages.HomePage;
+import gui.nopCommerceDemoStore.pages.LoginPage;
 import gui.nopCommerceDemoStore.pages.ProfilePage;
 import gui.nopCommerceDemoStore.pages.SignUpPage;
 import org.testng.annotations.AfterTest;
@@ -11,21 +12,28 @@ import org.testng.annotations.Test;
 
 public class SignUp_POM {
     private SHAFT.GUI.WebDriver driver;
+    private SHAFT.TestData.JSON testDate;
     private HomePage homePage;
     private SignUpPage signUpPage;
     private ProfilePage profilePage;
+    private LoginPage loginPage;
     private final String BASE_URL = "https://demo.nopcommerce.com/";
-
+    String user_email;
+    String user_password;
     @BeforeTest
     public void setUP()
     {
         // Open the Browser
         driver = new SHAFT.GUI.WebDriver(DriverFactory.DriverType.CHROME);
+        testDate = new SHAFT.TestData.JSON("simpleJSON.json");
+        // Open Web Application
+        driver.browser().navigateToURL(BASE_URL);
 
         // Initialize objects
         homePage = new HomePage(driver);
         signUpPage = new SignUpPage(driver);
         profilePage = new ProfilePage(driver);
+        loginPage = new LoginPage(driver);
     }
     @Test
     public void OpenApp()
@@ -54,6 +62,14 @@ public class SignUp_POM {
     {
         driver.browser().navigateToURL(BASE_URL);
         profilePage.addProductToCart();
+    }
+    @Test
+    public void testLoginPage()
+    {
+        user_email = testDate.getTestData("emailAddress");
+        user_password = testDate.getTestData("password");
+        homePage.navigateToLoginPage();
+        loginPage.Login(user_email,user_password);
     }
     @AfterTest
     public void tearDown()
